789a6d42d9561951c7c474abac3dcb216f8214ab
KhoaDD6 <dinhdangkhoa0201@gmail.com>
2021-06-23 10:01:36 +0700

    Config basic Spring MVC Thymeleaf

---
+++ SpringMVC_Thymealeaf/src/main/java/fsoft/cms/khoadd6/controller/IndexController.java
@@ -0,0 +1,1 @@
+package fsoft.cms.khoadd6.controller;import org.springframework.stereotype.Controller;import org.springframework.web.bind.annotation.GetMapping;@Controllerpublic class IndexController {    @GetMapping    public String index() {        return "index";    }}
